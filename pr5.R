2cd2defe8f9c416f9042159653f56fbb7c698706
Nuriaballes <nuriaballes16@gmail.com>
2023-03-07 12:02:19 +0100

    primeros ej

--- pr5.R
+++ pr5.R
@@ -1,1 +1,28 @@
-tygrfh
+#Ej.1
+numArtefactos<- c(17, 54, 10, 34, 90, 33, 49, 82, 12, 23, 56, 78, 44, 102, 10, 53, 4, 28, 37, 95)
+  #¿Cómo almacena los valores numéricos: integer o double?double
+  #Transforma el tipo de dato a número entero llamando al objeto ‘numArtefactos_int’. 
+numArtefactos_int <- as.integer(numArtefactos)
+#Ej.2. Calcula la media del objeto ‘numArtefactos_int’. 
+media_numArtefactos_int <- mean(numArtefactos_int)
+#Ej. 3.Calcula la mediana del objeto ‘numArtefactos_int’. Define brevemente la mediana: concepto y cálculo.
+mediana_numArtefactos_int <- median(numArtefactos_int)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
